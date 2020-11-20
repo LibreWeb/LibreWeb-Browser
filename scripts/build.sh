9a58be4e92e49d2c2e92e782abcff30f66cfc550
Melroy van den Berg <melroy@melroy.org>
2020-11-21 00:53:52 +0100

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# By: Melroy van den Berg
+# Description: Development build
+
+if [ ! -d "build" ]; then
+  echo "Creating build directory..."
+  mkdir build
+fi
+
+if [ -z "$(ls build)" ]; then
+  echo "INFO: Run cmake & ninja"
+  cd build
+  cmake -GNinja ..
+else
+  echo "INFO: Only run ninja..."
+  cd build
+fi
+ninja
