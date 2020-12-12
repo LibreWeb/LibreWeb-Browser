2a81a006f43a5b483530a1c92eabe0f62f644cbd
Melroy van den Berg <melroy@melroy.org>
2020-12-12 04:10:15 +0100

    Update git submodule again and disable Write-strings warning

--- src/ipfs.cc
+++ src/ipfs.cc
@@ -15,8 +15,13 @@ int IPFS::startIPFSDaemon()
     dup2(fd, 2); // ..and same with stderr
     close(fd);   // close fd
 
+    // Ignore ISO C++ forbids converting a string constant to ‘char*’
+    // This is valid in C.
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wwrite-strings"
     // stdout and stderr now write to /dev/null
     // Ready to call exec to start IPFS Daemon
-    constexpr char *proc[] = { "../../go-ipfs/ipfs", "daemon", "--init", "--migrate", 0};
+    constexpr char *proc[] = { "../../go-ipfs/ipfs", "daemon", "--init", "--migrate", NULL};
+    #pragma GCC diagnostic pop
     return execv(proc[0], proc);
 }
