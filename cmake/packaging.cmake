742a59c04f0a9255407c745b2dfb291da7186449
Melroy van den Berg <melroy@melroy.org>
2021-02-28 23:37:26 +0100

    Improve packaging

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -1,6 +1,6 @@
 
 # Example: https://github.com/MariaDB/server/tree/10.5/cmake
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Browser - Decentralized WWW")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "DWeb Browser - Decentralized Web-Browser")
 set(CPACK_PACKAGE_VENDOR "Melroy van den Berg")
 set(CPACK_PACKAGE_CONTACT "Melroy van den Berg <melroy@melroy.org>")
 set(CPACK_PACKAGE_HOMEPAGE_URL "https://melroy.org")
@@ -8,8 +8,8 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set(CPACK_PACKAGE_VERSION "${PROJECT_VERSION}")
 set(CPACK_SOURCE_PACKAGE_FILE_NAME "${PROJECT_TARGET}-${CPACK_PACKAGE_VERSION}")
-set(CPACK_DEBIAN_PACKAGE_SECTION "utils") # Change
-set(CPACK_RPM_PACKAGE_GROUP      "Applications/Productivity") # Change
+set(CPACK_DEBIAN_PACKAGE_SECTION "web")
+set(CPACK_RPM_PACKAGE_GROUP      "Applications/Internet")
 set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-v${CPACK_PACKAGE_VERSION}") # Without '-Linux' suffix
 
 if (${CMAKE_SYSTEM_NAME} MATCHES "Linux" AND EXISTS "/etc/os-release")
@@ -27,18 +27,19 @@ endif ()
 
 if(${LINUX_DISTRO} MATCHES "openSUSE")
   # OpenSuse/Leap
-  set(CPACK_RPM_PACKAGE_REQUIRES "") # Change
+  set(CPACK_RPM_PACKAGE_REQUIRES "gtkmm3")
 else()
   # Redhat/CentOS/Fedora/etc.
-  set(CPACK_RPM_PACKAGE_REQUIRES "") # Change
+  set(CPACK_RPM_PACKAGE_REQUIRES "gtkmm30")
 endif()
 # Optional RPM packages
-set(CPACK_RPM_PACKAGE_SUGGESTS "") # Change
+set(CPACK_RPM_PACKAGE_SUGGESTS "")
 
-# Debian/Ubuntu/Mint Mint
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5widgets5")
+# Debian Jessie/Ubuntu Trusty/Mint Qiana (libgtkmm-3.0-1) or 
+# Debian Stretch, Buster or newer, Ubuntu Xenial, Artful, Bionic or newer, Linux Mint Sarah, Tessa, Tina or newer (libgtkmm-3.0-1v5)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgtkmm-3.0-1 | libgtkmm-3.0-1v5")
 # Optional deb packages
-set(CPACK_DEBIAN_PACKAGE_SUGGESTS "") # Change
+set(CPACK_DEBIAN_PACKAGE_SUGGESTS "")
 
 # include CPack model once all variables are set
 include(CPack)
