981d311b0d3e0ae47fbae98f881c323f07da2566
Melroy van den Berg <melroy@melroy.org>
2021-03-06 01:06:26 +0100

    Insert one new line for quote, otherwise it will break the markdown syntax

--- src/draw.cc
+++ src/draw.cc
@@ -458,7 +458,7 @@ void Draw::make_quote()
         std::string line;
         while (std::getline(iss, line))
         {
-            buffer->insert_at_cursor("\n>" + line + "\n");
+            buffer->insert_at_cursor(">" + line + "\n");
         }
     }
     else
