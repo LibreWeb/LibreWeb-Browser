bf153f4baac4edc538afed2c8146156759531f92
Melroy van den Berg <melroy@melroy.org>
2021-08-17 16:27:53 +0200

    Update IPFS daemon to latest version

--- scripts/get_ipfs.sh
+++ scripts/get_ipfs.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-IPFS_VERSION=0.8.0
+# By: Melroy van den Berg
+#
+# Provide the IPFS version below
+
+IPFS_VERSION=0.9.1
+
+##############################
+# Leave the code alone below #
+##############################
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "INFO: Start downloading..."
+echo "INFO: Start downloading IPFS (version ${IPFS_VERSION})..."
 # Download & untar Go-IPFS
 wget --quiet "https://dist.ipfs.io/go-ipfs/v${IPFS_VERSION}/go-ipfs_v${IPFS_VERSION}_linux-amd64.tar.gz" -O "$CURRENT_DIR/go-ipfs.tar.gz"
 
