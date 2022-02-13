b81237455ec45afcf81b875341b4cf3721864f9c
Melroy van den Berg <melroy@melroy.org>
2022-02-13 23:32:00 +0100

    Improve shell scripts. Use _LIBRARY

--- scripts/build-macos-prod.sh
+++ scripts/build-macos-prod.sh
@@ -6,16 +6,12 @@ rm -rf build_prod_macos
 mkdir build_prod_macos
 cd build_prod_macos
 
-cmake -G Ninja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release .. &&
 # TODO:
 # cmake -G Xcode -DCODE_SIGN_IDENTITY="codesign ID..." -DDEVELOPMENT_TEAM_ID="team ID..."
-
-
-#TODO: xcodebuild \
-# -project "libreweb-browser.xcodeproj" \
-#    -scheme libreweb-browser \
-#    -configuration Release
-
-ninja && 
-echo "INFO: Start Drag & Drop packaging for MacOS...";
+# xcodebuild \
+#  -project "libreweb-browser.xcodeproj" \
+#  -scheme libreweb-browser \
+#  -configuration Release
+ninja &&
 cpack -C Release -G DragNDrop
