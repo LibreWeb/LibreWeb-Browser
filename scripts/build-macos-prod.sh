c86f642413b3edeebd50cc2d93cfad256c9df40c
Melroy van den Berg <melroy@melroy.org>
2022-02-13 23:03:55 +0100

    Add macOS runtime bins. Try to link the GTK Mac integration lib

--- scripts/build-macos-prod.sh
+++ scripts/build-macos-prod.sh
@@ -7,6 +7,15 @@ mkdir build_prod_macos
 cd build_prod_macos
 
 cmake -G Ninja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release ..
+# TODO:
+# cmake -G Xcode -DCODE_SIGN_IDENTITY="codesign ID..." -DDEVELOPMENT_TEAM_ID="team ID..."
+
+
+#TODO: xcodebuild \
+# -project "libreweb-browser.xcodeproj" \
+#    -scheme libreweb-browser \
+#    -configuration Release
+
 ninja && 
 echo "INFO: Start Drag & Drop packaging for MacOS...";
 cpack -C Release -G DragNDrop
