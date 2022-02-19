62663a466ab055d16ebee274a32b94d5050932cf
Melroy van den Berg <melroy@melroy.org>
2022-02-20 00:43:16 +0100

    Enable verbose?

--- scripts/build-macos-prod.sh
+++ scripts/build-macos-prod.sh
@@ -6,7 +6,8 @@ rm -rf build_prod_macos
 mkdir build_prod_macos
 cd build_prod_macos
 
-cmake -G Ninja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release .. &&
+# Enable verbose for debugging
+cmake -G Ninja -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release .. &&
 # TODO:
 # cmake -G Xcode -DCODE_SIGN_IDENTITY="codesign ID..." -DDEVELOPMENT_TEAM_ID="team ID..."
 # xcodebuild \
