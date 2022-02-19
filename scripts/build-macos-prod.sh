08dff5c51f5697a14c15dcc94809a1d49f1575fc
Melroy van den Berg <melroy@melroy.org>
2022-02-20 00:50:43 +0100

    Enable debug &trace

--- scripts/build-macos-prod.sh
+++ scripts/build-macos-prod.sh
@@ -6,8 +6,7 @@ rm -rf build_prod_macos
 mkdir build_prod_macos
 cd build_prod_macos
 
-# Enable verbose for debugging
-cmake -G Ninja -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release .. &&
+cmake -G Ninja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Release .. &&
 # TODO:
 # cmake -G Xcode -DCODE_SIGN_IDENTITY="codesign ID..." -DDEVELOPMENT_TEAM_ID="team ID..."
 # xcodebuild \
@@ -15,4 +14,5 @@ cmake -G Ninja -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DDOXYGEN:BOOL=FALSE -DCMAKE_BUI
 #  -scheme libreweb-browser \
 #  -configuration Release
 ninja &&
-cpack -C Release -G DragNDrop
+# Enable verbose for debugging
+cpack -V --trace -C Release -G DragNDrop
