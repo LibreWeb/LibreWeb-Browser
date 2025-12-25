982a6877990d7e9c21894bc915f326fa07b01594
Melroy van den Berg <melroy@melroy.org>
2025-12-25 22:09:58 +0100

    Just tell its a warning and skipping git archive command

--- scripts/create-source-archive.sh
+++ scripts/create-source-archive.sh
@@ -4,10 +4,13 @@
 # Depends on one environment variable: $CI_COMMIT_TAG
 
 if [ -z ${CI_COMMIT_TAG} ]; then
-    echo "ERROR: CI_COMMIT_TAG env. variable is not set! Do not build archive. Exiting..."
+    echo "WARNING: CI_COMMIT_TAG env. variable is not set! WARNING if this should become a new release."
+    echo
+    echo "Skipping git archive command..."
     exit 0
+else
+    # Create version.txt
+    echo -n "${CI_COMMIT_TAG}" > version.txt
+    # Create source archive (tar.gz)
+    git archive --format=tar.gz --output=build_prod/libreweb-browser-source-v${CI_COMMIT_TAG}.tar.gz --add-file=version.txt HEAD
 fi
-# Create version.txt
-echo -n "${CI_COMMIT_TAG}" > version.txt
-# Create source archive (tar.gz)
-git archive --format=tar.gz --output=build_prod/libreweb-browser-source-v${CI_COMMIT_TAG}.tar.gz --add-file=version.txt HEAD
