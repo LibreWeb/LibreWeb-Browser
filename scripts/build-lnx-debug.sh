16616cbf1cb62b7c271de7ae58b2da3f9f387b21
Melroy van den Berg <melroy@melroy.org>
2026-03-05 23:11:16 +0100

    Fux small typo in lnx debug script

--- scripts/build-lnx-debug.sh
+++ scripts/build-lnx-debug.sh
@@ -2,5 +2,5 @@
 # By: Melroy van den Berg
 # Description: Debug build for Linux
 rm -rf build
-make -GNinja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Debug -B build
+cmake -GNinja -DDOXYGEN:BOOL=FALSE -DCMAKE_BUILD_TYPE=Debug -B build
 cmake --build ./build --config Debug
